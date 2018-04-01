f7d91b9f374008bb3752c67c6056cb4687dd0eb4
enjoi <enjoi@192.168.1.14>
2018-04-01 12:21:26 -0400

    Added constructor for task, modify task by int, modify task by string

--- src/java/Task.java
+++ src/java/Task.java
@@ -1,7 +1,153 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author jmehl
  */
 public class Task {
     
+    private final String driver = "com.mysql.jdbc.Driver";
+    private final String connect = "jdbc:mysql://localhost:3306/CMSC495";
+    private final String user = "root"; // Change this to mysql username
+    private final String pword = "root"; // change this to mysql password 
+    
+    private String taskName;
+    private int priority;
+    private String taskDateCreated;
+    private int taskID;
+    private int projectID;
+    private int userID;
+    private String taskSummary;
+    private String taskDateEnded;
+    private Statement statement;
+    private PreparedStatement prepStatement;
+    private Connection con;
+    
+    
+        /**
+     *
+     * Create Task and create record in task table
+     * @param taskName - short name of task
+     * @param priority - integer ranking priority of task
+     * @param userID - user assigned to project
+     * @param projectID - unique ID to project
+     * @param taskID - unique ID to task
+     * @param taskSummary - string describing task in more detail
+     * @param taskDateCreated - date when task was created
+     * @param taskDateEnded - date task is due
+     */
+    
+    public Task(String taskName, 
+            int priority, int userID, 
+            int projectID, int taskID,
+            String taskSummary, String taskDateCreated,
+            String taskDateEnded){ // Constructor will insert a task record
+        
+        this.taskName = taskName;
+        this.priority = priority;
+        this.userID = userID;
+        this.projectID = projectID;
+        this.taskID = taskID;
+        this.taskSummary = taskSummary;
+        this.taskDateCreated = taskDateCreated;
+        this.taskDateEnded = taskDateEnded;
+        
+        try
+         {
+             Class.forName(driver);
+             con = DriverManager.getConnection(connect,user,pword);
+             
+             
+         
+             prepStatement = con.prepareStatement("Insert into Tasks values "
+                                                + "(?,?,?,?,?,?,?,?);");
+             prepStatement.setInt(1, taskID);
+             prepStatement.setInt(2, projectID);
+             prepStatement.setInt(3, userID);
+             prepStatement.setString(4, taskName);
+             prepStatement.setString(5, taskDateCreated);
+             prepStatement.setString(6, taskDateEnded);
+             prepStatement.setInt(7, priority);
+             prepStatement.setString(8,taskSummary);
+         
+             prepStatement.execute(); // perform insert
+             
+             con.close();
+         } 
+         catch (ClassNotFoundException | SQLException ex) 
+         {
+            System.out.println("An exception occurred");
+         }
+         
+        
+        
+    } // end constructor
+    
+    public void modifyTaskString(String colToModify, String newValue, 
+                               int taskID) 
+     {
+         
+         try
+         {        
+             Class.forName(driver);
+             con = DriverManager.getConnection(connect,user,pword);
+         
+             prepStatement = con.prepareStatement("UPDATE Tasks "
+                                                + "SET " + colToModify + " = ?" 
+                                                + "WHERE taskID = ?");
+
+         
+             prepStatement.setString(1, newValue);
+             prepStatement.setInt(2, taskID);
+         
+             prepStatement.execute(); // perform update
+             
+             con.close();
+         } 
+         catch (ClassNotFoundException | SQLException ex) 
+         {
+            System.out.println("An exception occurred");
+         }
+    } // end modifyTaskString()
+    
+    
+       /*
+     * Modify integer record in Project table
+     * @param colToModify - short name of project goal
+     * @param newValue - integer value to update in table
+     * @param tasktID - unique ID tracking project
+     */
+     public void modifyTaskInt(String colToModify, int newValue, 
+                               int taskID) 
+     {
+         
+         try
+         {        
+             Class.forName(driver);
+             con = DriverManager.getConnection(connect,user,pword);
+         
+             prepStatement = con.prepareStatement("UPDATE Tasks "
+                                                + "SET " + colToModify + "= ? " // Same as above method, setString adds " " to column name
+                                                + "WHERE taskID = ?;");
+
+             
+             prepStatement.setInt(1, newValue);
+             prepStatement.setInt(2, taskID);
+         
+             prepStatement.execute(); // perform update
+             
+             con.close();
+         } 
+         catch (ClassNotFoundException | SQLException ex) 
+         {
+            System.out.println("An exception occurred");
+         }
+    } // end modifyTaskInt()
 }
+
+
